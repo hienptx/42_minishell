8ddc9a617a7ba64478d87212fe95e82bb943408e
mafalda <https://github.com/hienptx>
2024-10-27 08:51:08 +0100

    fix: check syntax

--- srcs/syntax_check.c
+++ srcs/syntax_check.c
@@ -26,25 +26,30 @@ int	is_operator(const char *token)
 		|| ft_strcmp(token, "|") == 0);
 }
 
+static int	is_pipe_op(const char *tok)
+{
+	return (ft_strcmp(tok, "||") == 0 || ft_strcmp(tok, "|") == 0);
+}
+
+static int	is_redir_op(const char *tok)
+{
+	return (ft_strcmp(tok, ">") == 0 || ft_strcmp(tok, "<") == 0
+		|| ft_strcmp(tok, ">>") == 0 || ft_strcmp(tok, "<<") == 0);
+}
+
 int	check_syntax(char **tokens)
 {
 	int	i;
 
 	i = 0;
-	if (ft_strcmp(tokens[0], "|") == 0)
+	if ((is_operator(tokens[0]) && tokens[1] == NULL) || is_pipe_op(tokens[0]))
 		return (invalid_syntax_sms("Syntax error"));
 	while (tokens[i] != NULL)
 	{
-		if (is_operator(tokens[i]))
-		{
-			if (tokens[i + 1] == NULL
-				&& !(ft_strcmp(tokens[i], "|") == 0
-					&& (ft_strcmp(tokens[i + 1], ">") == 0
-						|| ft_strcmp(tokens[i + 1], ">>") == 0)))
-			{
-				return (invalid_syntax_sms("Syntax error"));
-			}
-		}
+		if (is_pipe_op(tokens[i]) && is_operator(tokens[i - 1]))
+			return (invalid_syntax_sms("Syntax error"));
+		if (is_redir_op(tokens[i]) && is_redir_op(tokens[i + 1]))
+			return (invalid_syntax_sms("Syntax error"));
 		i++;
 	}
 	return (0);
